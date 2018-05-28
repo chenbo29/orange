6bec1b0ec990698c2c6ad274e1abe75560d31f61
chenbo <chenbo@edusoho.com>
2018-05-28 18:45:41 +0800

    build basic 构造

--- bootstrap/SWBT.php
+++ bootstrap/SWBT.php
@@ -7,4 +7,7 @@
  */
 
 $container = new \Pimple\Container(require_once __DIR__ . '/../config/SWBT.php');
+$logger = new \Monolog\Logger('SWBT');
+$logger->pushHandler(new \Monolog\Handler\StreamHandler('php://output'));
+$container->logger = $logger;
 return $container;
--- src/SWBT.php
+++ src/SWBT.php
@@ -13,16 +13,27 @@ use Pimple\Container;
 
 class SWBT
 {
+    private $container;
     public function __construct(Container $container)
     {
+        $this->container = $container;
+        swoole_set_process_name('SWBT master');
     }
 
     public function run(){
-        echo __FILE__ . __LINE__;
+        $process = new \Swoole\Process(function (){
+            swoole_timer_after(5000, function () {
+                $this->container->logger->info('Hello World'.date('Y-m-d H:i:s', time()),['a'=>'d']);
+                $this->container->logger->info('Hello World'.date('Y-m-d H:i:s', time()),['a'=>'d']);
+            });
+        });
+        if (!$process) $this->container->logger->error("create process error", ['swoole_errno'=>swoole_errno, 'swoole_strerror'=>swoole_strerror]);
+        $process->name('SWBT child process');
+        $process->start();
     }
 
     public function __destruct()
     {
-        echo "\n";
+//        echo "\n";
     }
 }
