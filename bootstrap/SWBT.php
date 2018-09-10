dbe5dc41438ba70e8299f27fad4e42a9b3338439
chenbo <chenbo@edusoho.com>
2018-09-10 17:23:27 +0800

    fix bin/SWBT init

--- bootstrap/SWBT.php
+++ bootstrap/SWBT.php
@@ -6,27 +6,25 @@
  * Time: 下午2:18
  */
 date_default_timezone_set("Asia/Shanghai");
-$rootDir = __DIR__ . '/../';
-if (file_exists($rootDir . '/vendor')){
+$rootDir = dirname(__DIR__) . '/';
+if (file_exists($rootDir . 'vendor')){
     $swbtDir = $rootDir;
     $isIndependentProject = true;
 } else {
-    $rootDir .= '../../../';
     $swbtDir = $rootDir . 'swbt/';
-    if (!file_exists($rootDir)){
-        echo "Run vendor/bin/SWBT init\n";
+    if (!file_exists($swbtDir)){
+        echo "Run <bin/SWBT init>\n";
         exit;
     }
     $isIndependentProject = false;
 }
-
 if (file_exists($swbtDir . 'config/SWBT.php')){
     $container = new \Pimple\Container(require_once $swbtDir . 'config/SWBT.php');
     try {
-//        $logger = new \Monolog\Logger('SWBT');
-//        $logger->pushHandler(new \Monolog\Handler\StreamHandler('php://output'));
+        $logger = new \Monolog\Logger('SWBT');
+        $logger->pushHandler(new \Monolog\Handler\StreamHandler('php://output'));
+        // TODO 日志
         $pheanstalk = new Pheanstalk\Pheanstalk($container['beanstalkd']['host']);
-
         $fileSystem = new \Symfony\Component\Filesystem\Filesystem();
     } catch (Exception $e){
         echo $e->getMessage() . "\n";
@@ -41,5 +39,6 @@ if (file_exists($swbtDir . 'config/SWBT.php')){
 $container['root_dir'] = $rootDir;
 $container['swbt_dir'] = $swbtDir;
 $container['is_independent_project'] = $isIndependentProject;
+$container['swbt_dir_exist'] = file_exists($swbtDir);
 
 return $container;
--- src/SWBT.php
+++ src/SWBT.php
@@ -24,13 +24,13 @@ final class SWBT
     public function __construct(Container $container, $daemon = false)
     {
         $this->container           = $container;
-        $this->masterPidFilePath   = $container['swbt_dir'] . $this->container['pid']['file_path'];
         $this->fileSystem          = $container['fileSystem'];
-        $this->container['logger'] = function (){
+        $this->daemon              = $daemon;
+        $this->masterPidFilePath = $container['swbt_dir'] . $this->container['pid']['file_path'];
+        $this->container['logger'] = function () {
             return new Logger($this->container['log']['name']);
         };
-        $this->daemon              = $daemon;
-        if ($this->daemon){
+        if ($this->daemon) {
             $this->container['logger']->pushHandler(new StreamHandler($container['swbt_dir'] . $this->container['log']['file_path'] . '/' . date('Y-m-d') . '.log'));
             \Swoole\process::daemon();
         } else {
@@ -58,20 +58,6 @@ final class SWBT
         }
     }
 
-    public function init():void {
-        if (!$this->container['is_independent_project']) {
-            $swbtPath = $this->container['swbt_dir'];
-            $this->fileSystem->copy(dirname(__DIR__) . '/config/SWBT.php', $swbtPath . 'config/SWBT.php');
-            $paths = [$swbtPath, $swbtPath . 'config', $swbtPath . 'storage', $swbtPath . 'storage/logs'];
-            array_walk($paths, function ($path){
-                if (!file_exists($path)) {
-                    $this->fileSystem->mkdir($path);
-                } elseif (!is_writeable($path)) {
-                    $this->logger->error('Permission Denied', ['path' => $path]);
-                }
-            });
-        }
-    }
 
     private function swooleEvent($process){
 //        todo EventLoop暂无概念
